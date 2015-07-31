38b1f20021b2ff75315c479bf0d4f48b3b88777f
MKLau <matthewklau@fas.harvard.edu>
2015-07-31 10:29:55 -0400

    updated SNPer.sh

--- SNPer.sh
+++ SNPer.sh
@@ -9,21 +9,6 @@
 ### Setup
 ##############################
 
-#################################################################
-### Assume you are working locally (./) with an unzipped file ###
-#################################################################
-
-outdir=snps1238 #output directory
-demultiplex=false
-demultmove=false
-snp=false
-rdata=false
-pydata=true
-
-barcodes=~/scahan/Aphaenogaster_RADfiles/ddRAD1.txt #formatted: GCATG<tab>03A
-rads=/N/u/mklau/Mason/mklau/ads/SNPer/ddRAD1238  #unzipped file path
-demultfiles=/N/u/mklau/Mason/mklau/ads/SNPer/ddRAD1238
-=======
 #Assume you are working locally (./) with an unzipped file 
 outdir=ddRAD1 #output directory
 demultiplex=true
@@ -49,38 +34,29 @@ FASTQ=/N/soft/mason/galaxy-apps/fastx_toolkit_0.0.13/fastq_quality_filter
 #################################
 ### Demultiplex, trim and filter
 #################################
-
-if [ $demultiplex = "true" ]
-then
-    mkdir $outdir
-    cd $outdir
-    sabre se -m 1 -f $rads -b $barcodes -u unknown
-    cd ..
-elif [ $demultiplex = "false" ] && [ $demultmove = "true" ]
-then
-    mkdir $outdir
-    cd $outdir
-    cp $demultfiles/* .
-    cd ..
-fi ## end demultiplex
-
+mkdir $outdir
 cd $outdir
 
+if [ $demultiplex = "true" ]; then 
+    sabre se -m 1 -f $rads -b $barcodes -u unknown
+fi ## end demultiplex
+
 if [ $snp = "true" ]; then 
-    fqs=$(ls)
-    mkdir trimmed original
-    for X in $fqs;
-    do 
-	echo $X
-	$FASTX -Q33 -f 5 -l 95 -i $X -o ./trimmed/$X;
-	mv $X original/$X;
-    done
-    mkdir filtered
-    for X in $fqs;
-    do 
-	echo $X
-	$FASTQ -Q33 -q 10 -p 100  -i ./trimmed/$X -o ./filtered/$X;
-    done
+
+fqs=$(ls)
+mkdir trimmed original
+for X in $fqs;
+do 
+    echo $X
+    $FASTX -Q33 -f 5 -l 95 -i $X -o ./trimmed/$X;
+    mv $X original/$X;
+done
+mkdir filtered
+for X in $fqs;
+do 
+    echo $X
+    $FASTQ -Q33 -q 10 -p 100  -i ./trimmed/$X -o ./filtered/$X;
+done
 
 ### Filtered reads are used from here on
 
@@ -89,52 +65,39 @@ if [ $snp = "true" ]; then
 #################################
 
 ### map to "reference" genome
-    ln -s $refmap ./indexes
-    mkdir sam
-    for X in $fqs; 
-    do 
-	bowtie AphaenogasterRAD ./filtered/$X -S ./sam/$X.sam; 
-    done
+ln -s $refmap ./indexes
+mkdir sam
+for X in $fqs; 
+do 
+    bowtie AphaenogasterRAD ./filtered/$X -S ./sam/$X.sam; 
+done
 
 # ###Build the following as a bash script that will then be called as sh sam_map.sh
-    echo "#! /bin/bash" | tee "sam_map.sh"
-    echo "ref_map.pl \\" | tee -a "sam_map.sh"
-    for X in $fqs;
-    do 
-	if [ $X == "unknown" ]; then
-	    echo "Skipping unknown";
-	else 
-	    echo "-s ./sam/$X.sam \\" | tee -a "sam_map.sh";
-	fi
-    done
-    echo "-o ./output -n $nmismatch -m $mindepth -T $nthreads -b $batchid -S" | tee -a "sam_map.sh"
-    mkdir output
-    sh sam_map.sh
+echo "#! /bin/bash" | tee "sam_map.sh"
+echo "ref_map.pl \\" | tee -a "sam_map.sh"
+for X in $fqs;
+do 
+    if [ $X == "unknown" ]; then
+	echo "Skipping unknown";
+    else 
+	echo "-s ./sam/$X.sam \\" | tee -a "sam_map.sh";
+    fi
+done
+echo "-o ./output -n $nmismatch -m $mindepth -T $nthreads -b $batchid -S" | tee -a "sam_map.sh"
+mkdir output
+sh sam_map.sh
 
 ###############
 ### Genotyping
 ###############
 # genotypes -b $batchid -P ./output -r $minprog -m $minstack -t GEN -s
-    genotypes -b $batchid -P ./output -r $minprog -m $minstack -t GEN -s -c
-    genotypes -b $batchid -P ./output -r $minprog -m $minstack -t GEN -s -c
+genotypes -b $batchid -P ./output -r $minprog -m $minstack -t GEN -s -c
 fi #end snp
 
 ################################
 ### SNP Filtering and Output
 ################################
 if [ $rdata == "true" ]; then
-    INPUT=./output/batch_$batchid.haplotypes_$minprog.tsv
-    OUTPUT=./output/batch_$batchid.haplotypes_$minprog.gen
-    Rscript ../bin/haplotype_to_genepop/haplotype_to_genepop.R $INPUT $OUTPUT radtags
-fi
-
-if [ $pydata == "true" ]; then
-    INPUT=./output/batch_$batchid.haplotypes_$minprog.tsv
-    sed -i 's/atalog\ /atalog/g' $INPUT
-    python ../bin/make_genotype_file_for_adegenet.py $INPUT
-fi
-
-=======
     mkdir rdata
     INPUT=./output/batch_$batchid.haplotypes_$minprog.tsv
     OUTPUT=./rdata/batch_$batchid.haplotypes_$minprog.gen
