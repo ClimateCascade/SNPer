515e8cb23053a82ce367068353d77e86a919f0df
MKLau <matthewklau@fas.harvard.edu>
2015-08-03 08:38:53 -0400

    update

---
+++ src/getBars.R
@@ -0,0 +1,9 @@
+### isolate ddRAD barcodes
+
+library(gdata)
+bars <- lapply(3:7,function(i,xls) read.xls(xls=xls,sheet=i),xls='../docs/RADseq_flowcell_072815_barcode-masterlist.xlsx')
+names(bars) <- paste('ddRAD',c(4,5,6,7,9),sep='')
+
+## for (i in 1:length(bars)){bars[[i]][,2] <- paste(names(bars)[i],bars[[i]][,2],sep='')}
+
+lapply(1:length(bars),function(i,bars) write.table(bars[[names(bars)[i]]],file=paste(names(bars)[i],'barcodes',sep='.'),sep='\t',row.names=FALSE),bars=bars)
