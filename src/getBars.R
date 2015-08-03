82872c0a93f29ee38315d387e91ceac21488b3e1
MKLau <matthewklau@fas.harvard.edu>
2015-08-03 11:10:08 -0400

    fixed barcode quotes

--- src/getBars.R
+++ src/getBars.R
@@ -6,4 +6,4 @@ names(bars) <- paste('ddRAD',c(4,5,6,7,9),sep='')
 
 ## for (i in 1:length(bars)){bars[[i]][,2] <- paste(names(bars)[i],bars[[i]][,2],sep='')}
 
-lapply(1:length(bars),function(i,bars) write.table(bars[[names(bars)[i]]],file=paste('../docs/',names(bars)[i],'barcodes',sep='.'),sep='\t',row.names=FALSE),bars=bars)
+lapply(1:length(bars),function(i,bars) write.table(bars[[names(bars)[i]]],file=paste('../docs/',names(bars)[i],'.barcodes',sep=''),sep='\t',row.names=FALSE,quote=FALSE),bars=bars)
