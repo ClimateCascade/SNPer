f09763c5d8ea79838ec559ed150b84d451be848d
mklau <matthewklau@fas.harvard.edu>
2015-08-05 11:59:54 -0400

    finished markdown doc

--- src/global.R
+++ src/global.R
@@ -2,3 +2,27 @@ noNum <- function(x){
     x <- strsplit(x,split='')[[1]]
     paste(x[x %in% 0:9 == FALSE],collapse='')
 }
+
+getProj <- function(proj.id,flow,proj,ddRAD=FALSE){
+
+proj <- proj[proj[,proj.id] == 'x',]
+
+flow <- apply(flow,2,as.character)
+flow. <- toupper(flow)
+flow. <- sub(' ','',flow.)
+flow. <- sub('-','',flow.)
+flow. <- sub('_','',flow.);flow. <- sub('_','',flow.);flow. <- sub('_','',flow.)
+
+ids <- paste(proj$Site,proj$Collection_no,sep='')
+ids <- toupper(ids)
+ids <- sub(' ','',ids)
+ids <- sub('-','',ids)
+ids <- sub('_','',ids);ids <- sub('_','',ids);ids <- sub('_','',ids)
+
+if (ddRAD==FALSE){proj[ids %in% flow.,]}else{
+    sapply(ids[ids %in% flow.],
+           function(x,y) colnames(flow.)[apply(flow.,2,function(z,q) any(z == q),q=x)],
+           y=flow.)
+}
+
+}
