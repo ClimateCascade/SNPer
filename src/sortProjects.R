59d82109f20dfaa74ce77817698fd6de2a1f6262
MKLau <matthewklau@fas.harvard.edu>
2015-08-18 10:47:59 -0400

    debug sorting script

--- src/sortProjects.R
+++ src/sortProjects.R
@@ -1,6 +1,8 @@
 ### Sorting RADseqs by project
 ### MKLau 7Aug2015
 
+print('initiate')
+
 source('global.R')
 args <- commandArgs(TRUE)
 
@@ -15,18 +17,21 @@ seqs <- lapply(seqs,function(x) x[x != 'unknown' & grepl('ddRAD',x,ignore.case=T
 seqs[[7]] <- seqs[[7]][seqs[[7]] != 'APHPIC32.fq']
 names(seqs) <- paste('ddRAD',1:9,sep='')
 
+print('duplicate check')
 ## check for duplicates
 if (any(unlist(lapply(seqs,table)) != 1)){
     warning('Diplicate present')
     print(unlist(lapply(seqs,table))[unlist(lapply(seqs,table)) != 1])
 }
 
+
+print('load names')
+## Get the names of files based on matching with RAD-SEQ names
 master <- read.csv('../docs/RADseq_mastersheet_2014.csv')
 master <- master[master[,10] != '',]
 seqs <- unlist(seqs)
 dam <- read.csv('../docs/Data_allocation_mastersheet_01-26-15.csv')
 
-## Get the names of files based on matching with RAD-SEQ names
 seqs. <- sapply(unlist(sub('.fq','',seqs)),function(x) sapply(x,fixName))
 master.names <- as.character(sapply(paste(master[,2],master[,3],sep='_'),fixName))
 file.names <- character(0)
@@ -44,6 +49,7 @@ master <- master[is.na(file.names) == FALSE,]
 master.names <- master.names[is.na(file.names) == FALSE]
 file.names <- file.names[is.na(file.names) == FALSE]
 
+print('match project and seq names')
 ## get correct project names
 projects <- dam[,1:10]
 projects <- apply(projects,2,as.character)
@@ -68,6 +74,8 @@ project.names <- as.character(na.omit(project.names[(project.names %in% c('')) =
 colnames(master.id) <- apply(master.id,2,function(x) unique(x[x != ''])[1])
 master.id <- master.id[,is.na(colnames(master.id)) == FALSE]
 
+
+print('sorting')
 ## Sort by project
 
 #1 use project.names out.dir to make output folders
@@ -77,14 +85,18 @@ sapply(out.dir,dir.create)
 
 #2 use out.dir, project.names, master.id and file.names
 for (i in 1:length(project.names)){
+    in.files <- master.id[,colnames(master.id) == project.names[i]]
+    in.files <- file.names[in.files == project.names[i]]
     out.files <- master.id[,colnames(master.id) == project.names[i]]
-    out.files <- file.names[out.files == project.names[i]]
-    in.loc <- paste(in.dir,out.files,sep='/')
+    out.files <- master.names[out.files == project.names[i]]
+    in.loc <- paste(in.dir,in.files,sep='/')
     out.loc <- paste(out.dir[names(out.dir) == project.names[i]],out.files,sep='/')
     sys.cmd <- paste('cp',in.loc,out.loc)
+    print(project.names[i])
     sapply(sys.cmd,system)
 }
 
+print('checking output')
 #3 check output folder contents using dir and length
 out.check <- unlist(sapply(out.dir,function(x) length(dir(x)))) == apply(master.id[,colnames(master.id) %in% project.names],2,table)[2,]
 if (all(out.check)){print(all(out.check))}else{print(out.check)}
